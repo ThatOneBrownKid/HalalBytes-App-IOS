63d97036d89f60d8ad6a3f21b6d8c40221611376
ThatOneBrownKid <38635170+ThatOneBrownKid@users.noreply.github.com>
2024-02-22 21:18:29 -0500

    Login and Sign Up

--- HalalBytes App (IOS)/Core/Authenticate/LoginView.swift
+++ HalalBytes App (IOS)/Core/Authenticate/LoginView.swift
@@ -1,6 +1,6 @@
 //
 //  LoginView.swift
-//  HalalBytes App (IOS)
+//  HalalBytes
 //
 //  Created by Azwad Alam on 2/22/24.
 //
@@ -8,8 +8,72 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack{
+                //image
+                Image("halalbytes-red")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 100,height: 120)
+                    .padding(.vertical,32)
+                
+                
+                // form fields
+                VStack(spacing: 24){
+                    InputView(
+                        text: $email,
+                        title: "Email Address",
+                        placeholder: "name@example.com")
+                        .autocapitalization(.none)
+                    
+                    InputView(
+                        text: $password,
+                        title: "Password",
+                        placeholder: "Enter Your Password",
+                        isSecureField: true)
+                }
+                .padding(.horizontal)
+                .padding(.top, 12)
+                
+                
+                // sign in button
+                
+                Button {
+                    print("Log User in...")
+                } label: {
+                    HStack{
+                        Text("SIGN IN")
+                            .fontWeight(.semibold)
+                        Image(systemName: "arrow.right")
+                    }
+                    .foregroundColor(.white)
+                    .frame(width: UIScreen.main.bounds.width - 32, height: 48)
+                }
+                .background(Color(.systemBlue))
+                .cornerRadius(10)
+                .padding(.top, 24)
+                
+                Spacer()
+                
+                // sign up button
+                
+                NavigationLink{
+                    
+                } label: {
+                    HStack{
+                        Text("Dont have an account?")
+                        Text("Sign Up")
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    }
+                    .font(.system(size: 14))
+                }
+                
+            }
+        }
     }
 }
 

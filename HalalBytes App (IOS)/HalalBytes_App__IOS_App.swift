5b66c7a8ef3a6a1bcfc7059c5e8dbfcdc4ffd2f0
ThatOneBrownKid <38635170+ThatOneBrownKid@users.noreply.github.com>
2024-02-22 19:08:49 -0500

    Initial Commit

---
+++ HalalBytes App (IOS)/HalalBytes_App__IOS_App.swift
@@ -0,0 +1,17 @@
+//
+//  HalalBytes_App__IOS_App.swift
+//  HalalBytes App (IOS)
+//
+//  Created by Azwad Alam on 2/22/24.
+//
+
+import SwiftUI
+
+@main
+struct HalalBytes_App__IOS_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

11f88cb9e299741aa6a80f23f5a0afe446879904
ThatOneBrownKid <38635170+ThatOneBrownKid@users.noreply.github.com>
2024-02-23 00:52:04 -0500

    Create User

--- HalalBytes App (IOS)/App/HalalBytes_App__IOS_App.swift
+++ HalalBytes App (IOS)/App/HalalBytes_App__IOS_App.swift
@@ -6,10 +6,16 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct HalalBytes_App__IOS_App: App {
     @StateObject var viewModel = AuthViewModel()
+    
+    init(){
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
--- HalalBytes App (IOS)/Core/Authenticate/ViewModel/AuthViewModel.swift
+++ HalalBytes App (IOS)/Core/Authenticate/ViewModel/AuthViewModel.swift
@@ -7,21 +7,33 @@
 
 import Foundation
 import Firebase
+import FirebaseFirestoreSwift
 
+
+@MainActor
 class AuthViewModel: ObservableObject {
     @Published var userSession: FirebaseAuth.User?
     @Published var currentUser: User?
     
     init() {
-        
+        self.userSession = Auth.auth().currentUser
     }
     
     func signIn(withEmail email: String, password: String) async throws {
-        
+        print("Sign In...")
     }
     
     func createUser(withEmail email: String, password: String, fullname: String) async throws {
-        
+        do {
+            let result = try await Auth.auth().createUser(withEmail: email, password: password)
+            self.userSession = result.user
+            let user = User(id: result.user.uid, fullname: fullname, email: email)
+            let encodedUser = try Firestore.Encoder().encode(user)
+            try await Firestore.firestore().collection("users").document(user.id).setData(encodedUser)
+            print("user created")
+        } catch {
+            print("DEBUG: Failed to create user with error \(error.localizedDescription)")
+        }
     }
     
     func signOut() {
@@ -33,6 +45,7 @@ class AuthViewModel: ObservableObject {
     }
     
     func fetchUser() async {
+        guard let uid = Auth.auth().currentUser?.uid else {return}
         
     }
 }

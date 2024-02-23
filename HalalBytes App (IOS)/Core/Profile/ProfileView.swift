fc34ebd097fd5d54609705a46ab84a84753bb7cd
ThatOneBrownKid <38635170+ThatOneBrownKid@users.noreply.github.com>
2024-02-23 01:41:23 -0500

    Sign in and Sign out

--- HalalBytes App (IOS)/Core/Profile/ProfileView.swift
+++ HalalBytes App (IOS)/Core/Profile/ProfileView.swift
@@ -8,63 +8,67 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @EnvironmentObject var viewModel: AuthViewModel
     var body: some View {
-        List{
-            Section {
-                HStack {
-                    Text(User.MOCK_USER.initials)
-                        .font(.title)
-                        .fontWeight(.bold)
-                        .foregroundColor(.white)
-                        .frame(width: 72, height: 72)
-                        .background(Color(.systemGray3))
-                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-                    
-                    VStack (alignment: .leading, spacing: 4) {
-                        Text(User.MOCK_USER.fullname)
-                            .fontWeight(.semibold)
-                            .padding(.top, 4)
+        if let user = viewModel.currentUser {
+            List{
+                Section {
+                    HStack {
+                        Text(user.initials)
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .frame(width: 72, height: 72)
+                            .background(Color(.systemGray3))
+                            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
                         
-                        Text(User.MOCK_USER.email)
-                            .font(.footnote)
+                        VStack (alignment: .leading, spacing: 4) {
+                            Text(user.fullname)
+                                .fontWeight(.semibold)
+                                .padding(.top, 4)
+                            
+                            Text(user.email)
+                                .font(.footnote)
+                                .foregroundColor(.gray)
+                        }
+                    }
+                }
+                Section ("General"){
+                    HStack {
+                        SettingsRowView(imageName: "gear",
+                                        title: "Version",
+                                        tintColor: Color(.systemGray))
+                        
+                        Spacer()
+                        
+                        Text("1.0.0")
+                            .font(.subheadline)
                             .foregroundColor(.gray)
                     }
                 }
-            }
-            Section ("General"){
-                HStack {
-                    SettingsRowView(imageName: "gear",
-                                    title: "Version",
-                                    tintColor: Color(.systemGray))
+                Section ("Account"){
+                    Button {
+                        viewModel.signOut()
+                    } label: {
+                        SettingsRowView(imageName: "arrow.left.circle.fill",
+                                        title: "Sign Out",
+                                        tintColor: .red)
+                    }
                     
-                    Spacer()
-                    
-                    Text("1.0.0")
-                        .font(.subheadline)
-                        .foregroundColor(.gray)
+                    Button {
+                        print("Delete account..")
+                    } label: {
+                        SettingsRowView(imageName: "xmark.circle.fill",
+                                        title: "Delete Account",
+                                        tintColor: .red)
+                    }
                 }
-            }
-            Section ("Account"){
-                Button {
-                    print("Sign Out..")
-                } label: {
-                    SettingsRowView(imageName: "arrow.left.circle.fill",
-                                    title: "Sign Out",
-                                    tintColor: .red)
-                }
-                
-                Button {
-                    print("Delete account..")
-                } label: {
-                    SettingsRowView(imageName: "xmark.circle.fill",
-                                    title: "Delete Account",
-                                    tintColor: .red)
-                }
-            }
-        }
+            }        }
     }
 }
 
-#Preview {
-    ProfileView()
+struct ProfileView_Preview: PreviewProvider {
+    static var previews: some View{
+        ProfileView()
+    }
 }

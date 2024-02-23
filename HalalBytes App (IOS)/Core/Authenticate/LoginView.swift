5853d19d2a659376043457b755aaceb23a0b6276
ThatOneBrownKid <38635170+ThatOneBrownKid@users.noreply.github.com>
2024-02-22 19:50:51 -0500

    Login and Signup

---
+++ HalalBytes App (IOS)/Core/Authenticate/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  HalalBytes App (IOS)
+//
+//  Created by Azwad Alam on 2/22/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
---
+++ HalalBytes App (IOS)/Core/Authenticate/RegistrationView.swift
@@ -0,0 +1,18 @@
+//
+//  RegistrationView.swift
+//  HalalBytes App (IOS)
+//
+//  Created by Azwad Alam on 2/22/24.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegistrationView()
+}

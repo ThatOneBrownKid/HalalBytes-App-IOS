9f27eef1421ff437b59f4d3ae20e3fc3c3588887
ThatOneBrownKid <38635170+ThatOneBrownKid@users.noreply.github.com>
2024-03-27 01:35:59 -0400

    Added TabView, Home, and Resturant Pages

---
+++ HalalBytes App (IOS)/Core/Home/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  HalalBytes App (IOS)
+//
+//  Created by Azwad Alam on 3/27/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
